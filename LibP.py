5e636d49d1232acaa231eb388554fee5dcb4edc2
jeanne Champion <jeanne.champion@cpe.fr>
2022-11-29 09:31:22 +0100

    3e commit

--- LibP.py
+++ LibP.py
@@ -1,1 +1,20 @@
 #Fichier fonction
+
+import random
+Texte = "listemotpendu.txt"
+
+def RecupFichier(Texte):
+    liste=open(Texte)
+    listeL=liste.readlines()
+    listeS = listeL[0].split(",")
+    del listeS[-1]
+    return listeS
+
+def Alea(listeS):
+    nb= random.randint(0, len(listeS))
+    mot=listeS[nb]
+    return mot
+
+def affichermot(mot):
+    lettre=list(mot)
+    motT=[]
